8dbe067f34c80af9da79698ba8fbfdcd73d3e939
Hiram J. González <hiramjgonzalez98@gmail.com>
2020-12-21 19:36:00 -0400

    Implement CategoryList component

--- src/components/CategoryList/index.js
+++ src/components/CategoryList/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import ProductList from '../ProductList';
 import './styles.css';
 
+const fakeCategories = [
+    '¡Última chance!',
+    'Los más populares',
+    'Compra en dúo',
+    'Grupos pequeños',
+    'Grupos grandes'
+];
+
 const CategoryList = () => {
-    
+    return (
+        fakeCategories.map((title, index) => <ProductList key={index} listTitle={title} />)
+    ); 
 };
 
 export default CategoryList;
