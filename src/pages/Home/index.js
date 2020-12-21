6107f88d42b0f7b5cfcd43bdf9750052ca069058
Hiram J. González <hiramjgonzalez98@gmail.com>
2020-12-21 02:42:01 -0400

    Implement Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
+import Banner from '../../components/Banner';
+import CategoryList from '../../components/CategoryList';
 import './styles.css';
 
 const Home = () => {
-
     return (
-        <>
-            
-        </>
+        <div>
+            <Navbar />
+            <Banner />
+            <CategoryList />
+        </div>
     )
 };
 
