1c21c025e5ed395630a3388790dac076968c7890
Hiram J. González <hiramjgonzalez98@gmail.com>
2020-12-21 02:53:54 -0400

    Implement Banner component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './styles.css';
 
 const Banner = () => {
-    
+    return (
+        <div className='banner'>
+            <img src="https://placehold.co/1440x380?text=Banner" alt="Banner" />
+        </div>
+    )
 };
 
 export default Banner;
