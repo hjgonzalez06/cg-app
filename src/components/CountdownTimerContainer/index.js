1d1863ccf6a5821b57e6d154651536550b82a2c6
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-01-25 13:18:56 -0400

    Refactor CountdownTimer implementation

--- src/components/CountdownTimerContainer/index.js
+++ src/components/CountdownTimerContainer/index.js
@@ -1,32 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import dayjs from 'dayjs'
 
 import CountdownTimer from '../CountdownTimer'
+import calculateRemainingTime from '../../utils/calculateRemainingTime'
 
-const duration = require('dayjs/plugin/duration')
+const RECALCULATE_TIME = 1000
 
 const CountdownTimerContainer = ({ expirationDate }) => {
-  const calculateRemainingTime = () => {
-    dayjs.extend(duration)
-
-    const difference = dayjs(expirationDate).diff(dayjs())
-
-    if (difference <= 0) return 'EXPIRÓ'
-
-    return dayjs.duration({
-      hours: dayjs(expirationDate).diff(dayjs(), 'hours'),
-      minutes: dayjs(difference).minute(),
-      seconds: dayjs(difference).second()
-    }).format('HH:mm:ss')
-  }
-
-  const [remainingTime, setRemainingTime] = useState(() => calculateRemainingTime())
-  const RECALCULATE_TIME = 1000
+  const [remainingTime, setRemainingTime] = useState(() => calculateRemainingTime(expirationDate))
 
   useEffect(() => {
     const timerId = setInterval(() => {
-      setRemainingTime(() => calculateRemainingTime())
+      setRemainingTime(calculateRemainingTime(expirationDate))
     }, RECALCULATE_TIME)
 
     return () => clearInterval(timerId)
