2166a3840f849356e7447399a012bdaa6991a030
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-01-25 12:32:14 -0400

    Implement useProductList to get Products data

--- src/components/ProductListContainer/index.js
+++ src/components/ProductListContainer/index.js
@@ -2,68 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import ProductCard from '../ProductCard'
+import useProductList from '../../hooks/useProductList'
 
 import './styles.scss'
 
-const fakeProductsList = [
-  {
-    id: '1',
-    productPhotoUrl: 'https://placehold.co/310x210?text=Imagen',
-    listPrice: '$250.000,00',
-    discountedPrice: '$200.000,00',
-    discountPercentage: 20,
-    expirationDate: '2021-02-08',
-    description: 'Esta es un tarjeta de producto de prueba.',
-    colors: ['azul', 'blanco', 'negro'],
-    groupCapacity: 10,
-    subscriptorsNumber: 8,
-    occupancyPercentage: 80
-  },
-  {
-    id: '2',
-    productPhotoUrl: 'https://placehold.co/310x210?text=Imagen',
-    listPrice: '$250.000,00',
-    discountedPrice: '$200.000,00',
-    discountPercentage: 20,
-    expirationDate: '2021-02-15',
-    description: 'Esta es un tarjeta de producto de prueba.',
-    colors: ['azul', 'blanco', 'negro'],
-    groupCapacity: 10,
-    subscriptorsNumber: 8,
-    occupancyPercentage: 80
-  },
-  {
-    id: '3',
-    productPhotoUrl: 'https://placehold.co/310x210?text=Imagen',
-    listPrice: '$250.000,00',
-    discountedPrice: '$200.000,00',
-    discountPercentage: 20,
-    expirationDate: '2021-02-11',
-    description: 'Esta es un tarjeta de producto de prueba.',
-    colors: ['azul', 'blanco', 'negro'],
-    groupCapacity: 10,
-    subscriptorsNumber: 8,
-    occupancyPercentage: 80
-  },
-  {
-    id: '4',
-    productPhotoUrl: 'https://placehold.co/310x210?text=Imagen',
-    listPrice: '$250.000,00',
-    discountedPrice: '$200.000,00',
-    discountPercentage: 20,
-    expirationDate: '2021-02-09',
-    description: 'Esta es un tarjeta de producto de prueba.',
-    colors: ['azul', 'blanco', 'negro'],
-    groupCapacity: 10,
-    subscriptorsNumber: 8,
-    occupancyPercentage: 80
-  }
-]
-
 const ProductListContainer = ({ productIds, featuredProductId }) => {
+  const productList = useProductList()
+
   return (
     <div>
-      {fakeProductsList.map(({
+      {productList.map(({
         id,
         productPhotoUrl,
         listPrice,
