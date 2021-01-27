45bf515287020d5b6f8d51acaed120deff98f20a
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-01-27 11:00:23 -0400

    Remove alt attribute from Banner component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -5,7 +5,7 @@ import classes from './styles.scss'
 const Banner = () => {
   return (
     <div className={classes.banner}>
-      <img src="https://placehold.co/1440x380?text=Banner" alt="Banner" />
+      <img src="https://placehold.co/1440x380?text=Banner" alt='' />
     </div>
   )
 }
