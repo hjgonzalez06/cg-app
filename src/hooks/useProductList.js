9531ec6af9779117225e20987fcca2c39b28b38b
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-02-01 01:37:09 -0400

    Modify ProductList fake data

--- src/hooks/useProductList.js
+++ src/hooks/useProductList.js
@@ -1,10 +1,15 @@
+import Product1 from '../assets/product-imgs/Product1.svg'
+import Product2 from '../assets/product-imgs/Product2.svg'
+import Product3 from '../assets/product-imgs/Product3.svg'
+import Product4 from '../assets/product-imgs/Product4.svg'
+
 const useProductList = () => {
   const fakeProductsList = [
     {
       id: '1',
-      productPhotoUrl: 'https://placehold.co/310x210?text=Imagen',
-      listPrice: '$250.000,00',
-      discountedPrice: '$200.000,00',
+      productPhotoUrl: Product1,
+      listPrice: '$450',
+      discountedPrice: '$350',
       discountPercentage: '20%',
       expirationDate: '2021-02-08',
       description: 'Esta es un tarjeta de producto de prueba.',
@@ -15,9 +20,9 @@ const useProductList = () => {
     },
     {
       id: '2',
-      productPhotoUrl: 'https://placehold.co/310x210?text=Imagen',
-      listPrice: '$250.000,00',
-      discountedPrice: '$200.000,00',
+      productPhotoUrl: Product2,
+      listPrice: '$6.640',
+      discountedPrice: '$3.950',
       discountPercentage: '20%',
       expirationDate: '2021-02-15',
       description: 'Esta es un tarjeta de producto de prueba.',
@@ -28,9 +33,9 @@ const useProductList = () => {
     },
     {
       id: '3',
-      productPhotoUrl: 'https://placehold.co/310x210?text=Imagen',
-      listPrice: '$250.000,00',
-      discountedPrice: '$200.000,00',
+      productPhotoUrl: Product3,
+      listPrice: '$1.250',
+      discountedPrice: '$875',
       discountPercentage: '20%',
       expirationDate: '2021-02-11',
       description: 'Esta es un tarjeta de producto de prueba.',
@@ -41,9 +46,9 @@ const useProductList = () => {
     },
     {
       id: '4',
-      productPhotoUrl: 'https://placehold.co/310x210?text=Imagen',
-      listPrice: '$250.000,00',
-      discountedPrice: '$200.000,00',
+      productPhotoUrl: Product4,
+      listPrice: '$950',
+      discountedPrice: '$670',
       discountPercentage: '20%',
       expirationDate: '2021-02-09',
       description: 'Esta es un tarjeta de producto de prueba.',
