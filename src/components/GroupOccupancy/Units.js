2c19fecda23674e86b65d1db78e2ee0c84d3f494
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-02-05 23:51:33 -0400

    Implement GroupOccupancy icons

---
+++ src/components/GroupOccupancy/Units.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import solidMale from '../../assets/occupancy-icons/solid-male.svg'
+import outlineMale from '../../assets/occupancy-icons/outline-male.svg'
+
+import classes from './styles.module.scss'
+
+const _ = require('lodash')
+const MAX_FIGURES = 10
+
+const Units = ({ groupCapacity, occupancyPercentage }) => {
+  const amountOfPersonIcons = (groupCapacity < MAX_FIGURES) ? groupCapacity : MAX_FIGURES
+  const percentageNumber = parseInt(String(occupancyPercentage).replace('%', ''))
+  const amountFull = Math.floor(amountOfPersonIcons * (percentageNumber / 100))
+  const amountEmpty = amountOfPersonIcons - amountFull
+
+  return (
+    <div className={classes.occupancyIcons}>
+      {_.times(amountFull).map((_, index) => <img className={classes.personIcon} src={solidMale} key={index} alt='' />)}
+      {_.times(amountEmpty).map((_, index) => <img className={classes.personIcon} src={outlineMale} key={index} alt='' />)}
+    </div>
+  )
+}
+
+Units.propTypes = {
+  groupCapacity: PropTypes.number,
+  occupancyPercentage: PropTypes.string
+}
+
+export default Units
