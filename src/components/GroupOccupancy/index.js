1811b49eed06ea105dbd993be1fa3d873632f6dd
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-02-08 01:13:19 -0400

    Refactor GroupOccupancy styles

--- src/components/GroupOccupancy/index.js
+++ src/components/GroupOccupancy/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Units from './Units'
+
 import classes from './styles.module.scss'
 
 const GroupOccupancy = ({ groupCapacity, subscriptorsNumber, occupancyPercentage }) => {
   return (
     <div className={classes.groupOccupancy}>
-      <img src={`https://placehold.co/205x34?text=${subscriptorsNumber}`} alt='' />
+      <Units groupCapacity={groupCapacity} occupancyPercentage={occupancyPercentage} />
       <div className={classes.occupancyInfo}>
-        <h4 className={classes.occupancyPercentage}>{occupancyPercentage}</h4>
-        <h4 className={classes.occupancyText}>{`${subscriptorsNumber}/${groupCapacity} agrupados`}</h4>
+        <span>{occupancyPercentage}</span>
+        <span>{`${subscriptorsNumber}/${groupCapacity} agrupados`}</span>
       </div>
     </div>
   )
